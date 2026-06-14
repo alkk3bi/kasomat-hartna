87e5239cba3dc9194dbbdbd681606b4dd7e7d8a7
alk33bi <alk33bi@users.noreply.github.com>
2026-06-14 10:10:35 +0400

    Initial project deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // Set base path for GitHub Pages when building for production
-  base: mode === "development" ? "/" : "/kasomathartna/",
+  base: mode === "development" ? "/" : "/kasomat-hartna/",
   server: {
     host: "::",
     port: 8080,
